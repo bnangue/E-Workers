c2ea3fc509f9ad5d0cbd889dadcf789b4cb3813e
bnangue <bricenangue@gmail.com>
2017-09-02 09:54:00 +0200

    create project and task

---
+++ app/src/main/java/com/bricenangue/insyconn/e_workers/activity/CreateProjectActivity.java
@@ -0,0 +1,12 @@
+package com.bricenangue.insyconn.e_workers.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
